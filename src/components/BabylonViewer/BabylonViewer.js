5c53fc638f51bffefc8341bb111706fcad9dd551
Interface0625 <karolis.lukosius@gmail.com>
2020-08-26 13:57:58 +0200

    update

--- src/components/BabylonViewer/BabylonViewer.js
+++ src/components/BabylonViewer/BabylonViewer.js
@@ -9,24 +9,19 @@ const onSceneReady = (scene)=>{
   glbl_scene = scene
   const camera = new FreeCamera("cameraID", new Vector3(0,3,5), scene)
   const canvas = scene.getEngine().getRenderingCanvas()
-  camera.setTarget(new Vector3())
+  camera.setTarget(new Vector3(0,1,0))
   camera.attachControl(canvas, true)
   const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-  SceneLoader.Append("http://localhost:8888/", "dance.glb", scene, function (scene) {
-    // do something with the scene
-    // http://83.251.240.178:3000/
-  });
+  SceneLoader.Append("http://localhost:8888/", "dance.glb", scene, function (scene) { });
 }
+
 const onRender = ()=>{}
 
 function BabylonsViewer( props ) {
   console.log(props.filename)
   if(glbl_scene && props.filename){
     glbl_scene.meshes.forEach(m=>m.dispose())
-    SceneLoader.Append("http://localhost:8888/", props.filename, glbl_scene, function (scene) {
-      // do something with the scene
-      // http://83.251.240.178:3000/
-    });
+    SceneLoader.Append("http://localhost:8888/", props.filename, glbl_scene, function (scene) { });
   }
   return (<React.Fragment>
     <SceneComponent antialias onSceneReady={ onSceneReady } onRender={ onRender } id="my-canvas" />
